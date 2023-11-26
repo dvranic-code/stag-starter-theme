f1be690845a2f416b239dd3ab6c8e315e29066e2
Dejan Rudic Vranic <dejanrudicvranic@gmail.com>
2023-11-26 13:31:10 +0100

    usluge meta

--- src/CustomPostTypes/class-stag-cpt-usluge.php
+++ src/CustomPostTypes/class-stag-cpt-usluge.php
@@ -22,6 +22,7 @@ if ( ! class_exists( 'Stag_CPT_Usluge' ) ) {
 		 */
 		public function __construct() {
 			add_action( 'init', array( $this, 'register_post_type' ) );
+			add_action( 'init', array( $this, 'register_taxonomy' ) );
 		}
 		/**
 		 * Register Custom Post Type
@@ -56,24 +57,64 @@ if ( ! class_exists( 'Stag_CPT_Usluge' ) ) {
 				'label'               => __( 'Usluga', 'stag' ),
 				'description'         => __( 'Usluga Description', 'stag' ),
 				'labels'              => $labels,
-				'supports'            => array( 'title', 'editor', 'thumbnail', 'revisions', 'custom-fields' ),
+				'supports'            => array( 'title', 'custom-fields' ),
 				'hierarchical'        => false,
-				'public'              => true,
+				'public'              => false,
 				'show_ui'             => true,
 				'show_in_menu'        => true,
 				'menu_position'       => 5,
-				'menu_icon'           => 'dashicons-portfolio',
+				'menu_icon'           => 'dashicons-code-standards',
 				'show_in_admin_bar'   => true,
 				'show_in_nav_menus'   => true,
 				'can_export'          => true,
-				'has_archive'         => 'usluge',
-				'exclude_from_search' => false,
-				'publicly_queryable'  => true,
-				'capability_type'     => 'page',
+				'has_archive'         => false,
+				'exclude_from_search' => true,
+				'publicly_queryable'  => false,
+				'capability_type'     => 'post',
 				'show_in_rest'        => true,
 			);
 
 			register_post_type( 'usluge', $args );
+		}
+
+		/**
+		 * Register Custom Taxonomy
+		 */
+		public function register_taxonomy() {
+			$labels = array(
+				'name'                       => _x( 'Tipovi Dijagnostike', 'Taxonomy General Name', 'stag' ),
+				'singular_name'              => _x( 'Tip Dijagnostike', 'Taxonomy Singular Name', 'stag' ),
+				'menu_name'                  => __( 'Tipovi Dijagnostike', 'stag' ),
+				'all_items'                  => __( 'Svi Tipovi Dijagnostike', 'stag' ),
+				'parent_item'                => __( 'Roditeljski Tip Dijagnostike', 'stag' ),
+				'parent_item_colon'          => __( 'Roditeljski Tip Dijagnostike:', 'stag' ),
+				'new_item_name'              => __( 'Novi Naziv Tipa Dijagnostike', 'stag' ),
+				'add_new_item'               => __( 'Dodaj Novi Tip Dijagnostike', 'stag' ),
+				'edit_item'                  => __( 'Uredi Tip Dijagnostike', 'stag' ),
+				'update_item'                => __( 'Ažuriraj Tip Dijagnostike', 'stag' ),
+				'view_item'                  => __( 'Pogledaj Tip Dijagnostike', 'stag' ),
+				'separate_items_with_commas' => __( 'Odvoji Tipove Dijagnostike zarezima', 'stag' ),
+				'add_or_remove_items'        => __( 'Dodaj ili ukloni Tipove Dijagnostike', 'stag' ),
+				'choose_from_most_used'      => __( 'Izaberi iz najkorišćenijih', 'stag' ),
+				'popular_items'              => __( 'Popularni Tipovi Dijagnostike', 'stag' ),
+				'search_items'               => __( 'Pretraži Tipove Dijagnostike', 'stag' ),
+				'not_found'                  => __( 'Nije pronađeno', 'stag' ),
+				'no_terms'                   => __( 'Nema Tipova Dijagnostike', 'stag' ),
+				'items_list'                 => __( 'Lista Tipova Dijagnostike', 'stag' ),
+				'items_list_navigation'      => __( 'Navigacija kroz listu Tipova Dijagnostike', 'stag' ),
+			);
+
+			$args = array(
+				'labels'            => $labels,
+				'hierarchical'      => true,
+				'public'            => false,
+				'show_ui'           => true,
+				'show_admin_column' => true,
+				'show_in_nav_menus' => true,
+				'show_in_rest'      => true,
+			);
+
+			register_taxonomy( 'tip_dijagnostike', array( 'usluge' ), $args );
 		}
 	}
 }
